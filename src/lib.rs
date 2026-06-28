8c43a04a373dbc58e971f810986c3caab223bc2a
Daniel Marschall <28412477+danielmarschall@users.noreply.github.com>
2026-06-28 22:04:58 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,19 @@ use std::os::raw::c_char;
 
 #[no_mangle]
 pub extern "C" fn rd_get_id() -> *mut c_char {
+    let result = std::panic::catch_unwind(|| {
+        let id = Config::get_id();
 
-    let id = Config::get_id();
+        CString::new(id)
+            .ok()
+            .map(|s| s.into_raw())
+            .unwrap_or(std::ptr::null_mut())
+    });
 
-    CString::new(id)
-        .unwrap()
-        .into_raw()
-
+    match result {
+        Ok(ptr) => ptr,
+        Err(_) => std::ptr::null_mut(),
+    }
 }
 
 #[no_mangle]
